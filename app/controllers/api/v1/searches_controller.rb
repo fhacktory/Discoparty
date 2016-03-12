64651bd6f5b41fbe80860b616552d72559881b94
kevcha <kevin@craftsmen.io>
2016-03-12 19:18:40 +0100

    Add a searches controller in a api v1 namespace

---
+++ app/controllers/api/v1/searches_controller.rb
@@ -0,0 +1,12 @@
+module Api
+  module V1
+    class SearchesController < ::ApplicationController
+      def show
+        if params[:q].present?
+          tracks = DeezerApi.search(params[:q]) + SoundcloudApi.search(params[:q]) + YoutubeApi.search(params[:q])
+          render json: tracks
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resource :search, only: :show
+    end
+  end
+
   resources :playlists, only: [:create, :show] do
     member do
       resources :tracks, only: :create
