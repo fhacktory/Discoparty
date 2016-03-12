910fcb41c24d97419627df99a2dc7d460b71e950
kevcha <kevin@craftsmen.io>
2016-03-13 00:35:07 +0100

    Add LovesController and HatesController to handle track votes

---
+++ app/controllers/api/v1/loves_controller.rb
@@ -0,0 +1,32 @@
+module Api
+  module V1
+    class LovesController < ::ApplicationController
+      def create
+        track = Track.find(params[:track_id])
+        if Vote.create(love_params.merge(track: track))
+          render status: :created
+        else
+          render status: :bad_request
+        end
+      end
+
+      def destroy
+        vote = Vote.find(params[:id])
+        if vote.destroy
+          render status: :ok
+        else
+          render status: :bad_request
+        end
+      end
+
+      private
+
+      def love_params
+        {
+          vote_type: 'love',
+          user: current_user
+        }
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resource :search, only: :show
       resources :playlists, only: :show do
-        resources :tracks, only: :create
+        resources :tracks, only: :create do
+          resources :loves, only: [:create, :destroy]
+          resources :hates, only: [:create, :destroy]
+        end
       end
     end
   end
