869c6f2c8b1f830c38e75508cad5c87fffe43e30
kevcha <kevin@craftsmen.io>
2016-03-13 03:25:16 +0100

    Add default image for tracks without one

--- app/serializers/playlist_serializer.rb
+++ app/serializers/playlist_serializer.rb
@@ -1,4 +1,6 @@
 class PlaylistSerializer < ActiveModel::Serializer
+  include ActionView::Helpers::AssetUrlHelper
+
   attributes :name, :played_at, :created_at, :updated_at, :tracks
 
   def tracks
@@ -10,7 +12,7 @@ class PlaylistSerializer < ActiveModel::Serializer
         provider_track_id: track.provider_track_id,
         duration: track.duration,
         artist: track.artist,
-        image_url: track.image_url,
+        image_url: image_for(track),
         score: track.score,
         loved: track.love_for(scope).present?,
         hated: track.hate_for(scope).present?,
@@ -18,5 +20,11 @@ class PlaylistSerializer < ActiveModel::Serializer
         hate_url: api_v1_playlist_track_hates_path(object, track)
       }
     end.sort_by { |track| track[:score] }
+  end
+
+  private
+
+  def image_for(track)
+    track.image_url.present? ? track.image_url : image_path('default-song-icon-2x.png')
   end
 end
